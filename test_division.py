9d0c5961b2bf0e415e8ed3e952933ba5906809a7
voltage-vulnerability <75689221+voltage-vulnerability@users.noreply.github.com>
2026-06-28 19:52:45 +0530

    Create test_division.py
    added the pytest division file

---
+++ test_division.py
@@ -0,0 +1,7 @@
+from myapp import safe_division
+
+def test_normal_division():
+  assert safe_division(100,5) == 20.0
+
+def test_zero_division():
+  assert safe_division(10,0) == 'Division by zero is not allowed'
