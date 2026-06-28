6adc42c2f369deb2d513b941a6a1c4b3ca6dc0f9
voltage-vulnerability <75689221+voltage-vulnerability@users.noreply.github.com>
2026-06-28 20:10:55 +0530

    Update test_multiply.py

--- test_multiply.py
+++ test_multiply.py
@@ -3,4 +3,4 @@ from myapp import safe_multiply
 def zero_multipication():
   assert safe_multiply(0,0) == 'Answer is zero.'
 def normal_multiplication():
-  assert safe_multiply(10,90) == '900'
+  assert safe_multiply(10,90) == 900
