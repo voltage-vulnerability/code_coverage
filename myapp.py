feb1d778c12efe77367b32950cf992f9ffb52dda
voltage-vulnerability <75689221+voltage-vulnerability@users.noreply.github.com>
2026-06-28 19:58:01 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,9 +8,3 @@ def safe_multiply(a,b):
   if a == 0 or b == 0:
     return "Answer is zero."
   return a * b  
-print(safe_division(10,2))
-print(safe_division(10,0))
-
-print(safe_multiply(0,0))
-print(safe_multiply(10,1))
-print(safe_multiply(11,0))
