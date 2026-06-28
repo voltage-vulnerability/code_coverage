c2411567da5331023730a60248d2cee98667e255
voltage-vulnerability <75689221+voltage-vulnerability@users.noreply.github.com>
2026-06-28 21:29:41 +0530

    Update test_multiply.py

--- test_multiply.py
+++ test_multiply.py
@@ -4,5 +4,5 @@ def tes_zero_multipication():
   assert safe_multiply(1,0) == 'Answer is zero.'
 def test_normal_multiplication():
   assert safe_multiply(10,90) == 900
-def tes1_zero1_multipication():
+def test_zero1_multipication():
   assert safe_multiply(0,10) == 'Answer is zero.'
